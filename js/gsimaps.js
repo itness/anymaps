108037fcfb5b5b8fa092553199d865f13f0677b0
itness <itness0000@gmail.com>
2016-04-26 21:02:11 +0900

    add my layers

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -28,6 +28,7 @@ CONFIG.layers = [
 	,'./layers_txt/layers3.txt'
 	,'./layers_txt/layers4.txt'
 	,'./layers_txt/layers5.txt'
+	,'./layers_txt/layers6.txt'
     ,'./layers_txt/layers_experimental.txt'
 ];
 
